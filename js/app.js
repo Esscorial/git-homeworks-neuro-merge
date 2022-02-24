b5a369b4425358a2f723ab3ddf72f1f6fe93fcae
Esscorial <98604447+Esscorial@users.noreply.github.com>
2022-02-24 22:01:09 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
 (() => {
     const earlyorderForm = document.querySelector('#earlyorder-form');
-
     earlyorderForm.addEventListener('submit', ev => {
         ev.preventDefault();
-
         const earlyorderContact = earlyorderForm.querySelector('#earlyorder-contact');
         const contact = earlyorderContact.value.trim();
-
         if (contact === '') {
             return;
         }
-        
         // TODO: send email simulation:
         setTimeout(() => {
             $('#earlyorderModal').modal('show');
